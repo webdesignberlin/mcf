3938a2a728563b0264bc8b37117909b120e168aa
Frank Holder <fholder@ws00330.int.mcfit.com>
2015-01-05 18:23:07 +0100

    added karma for unit testing

---
+++ grunt/karma.js
@@ -0,0 +1,75 @@
+/**
+ * + Task Config: grunt-karma
+ * https://github.com/karma-runner/grunt-karma
+ * =====================================================================
+ */
+
+module.exports = {
+
+  unit: {
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    //  List of files/patterns to load in the browser
+    options: {
+      files: [
+        'app/assets-dev/js/*.js',
+        'test/unit/*Spec.js'
+      ]
+    },
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: OFF, ERROR, WARN, INFO, DEBUG
+     logLevel: 'INFO',
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    // Chrome, PhantomJS
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+
+  }
+
+};
---
+++ test/unit/test.Spec.js
@@ -0,0 +1,9 @@
+describe("A suite is just a function", function() {
+
+  it("and so is a spec", function() {
+
+    expect(a).toBe(true);
+    expect(b).toBe(false);
+
+  });
+});
